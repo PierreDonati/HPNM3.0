12b1d8edd5fc9b54f0ca3eaa699f7c2c328fac22
Pierre Donati <pierre.donati_arranz@edu.escpeurope.eu>
2018-09-13 17:28:48 +0700

    populating users and parties seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+pierre = User.new(email: "pierre@gmail.com", password: "123456", first_name: "Pierre", last_name: "Donati", age: 24, phone_number: "0785154005")
+pierre.save!
+
+jean = User.new(email: "jean@gmail.com", password: "123456", first_name: "Jean", last_name: "Donati", age: 21, phone_number: "0787976")
+jean.save!
+
+matthieu = User.new(email: "matthieu@gmail.com", password: "123456", first_name: "Matthieu", last_name: "Donati", age: 18, phone_number: "077688678")
+matthieu.save!
+
+luc = User.new(email: "luc@gmail.com", password: "123456", first_name: "Luc", last_name: "Donati", age: 26, phone_number: "076897579")
+luc.save!
+
+paul = User.new(email: "paul@gmail.com", password: "123456", first_name: "Paul", last_name: "Donati", age: 23, phone_number: "07766897")
+paul.save!
+
+
+
+
+party1 = Party.new(user: pierre, name: "Murge dans mon antre", address: "5 rue pétion", date: "mercredi prochain", number_guests: "7", description: "On se retourne la tête", requirement: "bières, rhum, gin")
+party1.save!
+
+party2 = Party.new(user: jean, name: "Soirée barbeuc", address: "8 rue saint-maur", date: "jeudi dans 2 semaines", number_guests: "4", description: "Petit barbeux tranquille", requirement: "viande, bières")
+party2.save!
+
+party3 = Party.new(user: matthieu, name: "Soirée classico", address: "40 Avenue de la République", date: "Samedi soir 22h", number_guests: "5", description: "gros classico pour les fans de foot", requirement: "bières, chips")
+party3.save!
+
+party4 = Party.new(user: pierre, name: "Soirée cinéma", address: "10 Rue de Ménilmontant", date: "Dimanche début de soirée (18h)", number_guests: "5", description: "Projection de film et débat", requirement: "libres à vous de ramener ce que vous souhaitez")
+party4.save!
+
+
+
+
