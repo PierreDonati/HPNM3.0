f31ec71f90c42bf19ccb7065d8987d0765590aac
Pierre Donati <pierre.donati_arranz@edu.escpeurope.eu>
2018-09-19 22:49:24 +0700

    validation on people coming added but no message displayed in case of error

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,7 +3,19 @@ class Booking < ApplicationRecord
   belongs_to :party
 
   validates :people_coming, presence: true
+  validate :maximum_people_coming
   validates :description, presence: true
   validates :status, inclusion: {in: ['pending', 'accepted', 'declined']}
 
+
+private
+
+def maximum_people_coming
+  if self.people_coming > self.party.available_seats
+    errors.add(:people_coming, "Le nombre de personnes venant ne peut pas excéder le nombre de places disponibles")
+  end
 end
+
+end
+
+
